d01886a4ee5520fd592f572fc1ee31589cbfa56f
koastiebratt@gmail.com <you@example.com>
2020-09-01 07:30:44 -0400

    Small Adds

--- src/part1/Menu.java
+++ src/part1/Menu.java
@@ -1,5 +1,17 @@
 package part1;
 
 public class Menu {
-
+	
+	public static String Print_Menu() {
+		
+		String user_Choice;
+		
+		//Print new Menu
+		System.out.println();
+		
+		//Get Use choice from scanner
+		return user_Choice;
+		
+		
+	}
 }
--- src/part1/Winner.java
+++ src/part1/Winner.java
@@ -1,5 +1,17 @@
 package part1;
 
 public class Winner {
-
+	
+	public static boolean checkWinner(int playerScore, int dealerScore, boolean auto_lost){
+		
+		if ((playerScore > dealerScore && playerScore <=21) && !auto_lost) {
+			System.out.println("Player Wins");
+			return true;
+		} else if ((playerScore < dealerScore && dealerScore <=21) && !auto_lost) {
+			System.out.println("Dealer Wins");
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
