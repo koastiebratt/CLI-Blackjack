52a98c21329fd4d6aca1daffb4da6adba15216da
koastiebratt@gmail.com <you@example.com>
2020-09-03 19:24:00 -0400

    Double Dos

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -5,12 +5,24 @@ public class Main {
 	public static void main(String[] args) {
 		//Gameplay Logic goes here
 		String userInput = "";
+		
 		do {
+			
+			int playerScore = 0;
+			
+			do {
+				
+			
 			userInput = Menu.GameMenu();
 			if (userInput == "Y") {
 				userInput = "";
 				
-			}
+				}
+			
+			//Run players Turn while under
+			}while(playerScore < 21);
+		
+		//While Player wants to play
 		} while (userInput == "Y");
 		
 		
